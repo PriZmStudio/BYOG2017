60946a684b29ec5221db37e49eda00460b9e215b
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2017-11-08 01:59:45 +0530

    Final NGDC Build
    Signed-off-by: RudraNilBasu <rudra.nil.basu.1996@gmail.com>

--- BYOG2017/BYOG2017/Assets/Scripts/PlayerController.cs
+++ BYOG2017/BYOG2017/Assets/Scripts/PlayerController.cs
@@ -13,12 +13,13 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 touchOrigin = -Vector2.one;
 
-    [SerializeField]
-    TMPro.TextMeshProUGUI test_text;
+    private bool btn_left_pressed, btn_right_pressed;
 
     // Use this for initialization
     void Start()
     {
+        btn_left_pressed = false;
+        btn_right_pressed = false;
         motor = gameObject.GetComponent<PlayerMotor>();
     }
 
@@ -29,8 +30,7 @@ public class PlayerController : MonoBehaviour
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
         speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;// * Time.deltaTime;
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-        print("Touched: " + Input.touchCount);
-        test_text.text = "Touched: " + Input.touchCount;
+        /*
         if (Input.touchCount == 1) {
             Touch touchInput = Input.touches[0];
             if (touchInput.position.x < Screen.width / 2) {
@@ -39,6 +39,17 @@ public class PlayerController : MonoBehaviour
                 speedX = 1 * moveSpeed;
             }
         }
+        */
+        if (!(btn_left_pressed && btn_right_pressed)) {
+            if (btn_left_pressed) {
+                speedX = -1;
+            } else if (btn_right_pressed) {
+                speedX = 1;
+            } else {
+                speedX = 0;
+            }
+        }
+        speedX *= moveSpeed;
 #endif
 
         motor.MoveBody(speedX);
@@ -48,9 +59,34 @@ public class PlayerController : MonoBehaviour
             motor.jump(jumpSpeed);
         }
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-        if (Input.touchCount == 1) {
+        /*
+        if (Input.touchCount == 2) {
+            motor.jump(jumpSpeed);
+        }
+        */
+        if (btn_left_pressed && btn_right_pressed) {
             motor.jump(jumpSpeed);
         }
 #endif
+    }
+
+    public void btn_left_press_down()
+    {
+        btn_left_pressed = true;
+    }
+
+    public void btn_left_press_up()
+    {
+        btn_left_pressed = false;
+    }
+
+    public void btn_right_press_down()
+    {
+        btn_right_pressed = true;
+    }
+
+    public void btn_right_press_up()
+    {
+        btn_right_pressed = false;
     }
 }
